294fdddd7eeaf21d992d73c4497849d9cce322ab
Smileman1 <ejrwnd1103@naver.com>
2021-10-11 22:27:48 +0900

    Num1.py 코드개선

--- codingTest/programmers/Python/Num1.py
+++ codingTest/programmers/Python/Num1.py
@@ -3,6 +3,9 @@ def solution(citations):
     citations.sort()
     abc = {}
     lenCitations = len(citations)
+    answerLow = 0
+    answerHigh = lenCitations
+    answerKey = -1
 
     for x in citations:
         if x not in abc.keys():
@@ -13,13 +16,31 @@ def solution(citations):
     a = list(abc.keys())
     for x in range(len(a)):
         lowNum = 0
-        for y in range(x):
+        for y in range(x+1):
             lowNum += abc[a[y]]
         highNum = lenCitations - lowNum
         if highNum >= a[x] and lowNum <= a[x]:
             answer = a[x]
+            answerKey = x
+            answerLow = lowNum
+            answerHigh = highNum
+
+    if answerKey < len(a)-1:
+        if answerKey == -1:
+            for x in range(0,a[0]):
+                if answerHigh >= x and answerLow <= x:
+                    answer = x
+                else:
+                    break
+        else:
+            for x in range(a[answerKey], a[answerKey+1]):
+                print(answerHigh)
+                print(x)
+                if answerHigh >= x and answerLow <= x:
+                    answer = x
+                else: break
 
     return answer
 
 
-print(solution([660, 48, 7, 402, 969, 639, 432, 124, 461, 909, 92, 594, 891, 702, 298, 936, 62, 940, 946, 505]))
+print(solution([660]))
